c4a44e1392bb39128961663942ea637146a92156
abuchijuliet <abuchijuliet44@gmail.com>
2023-02-27 10:36:41 +0000

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,70 +1,73 @@
 #include "sort.h"
+
+
 /**
- * quick_sort - quicksort algorithm
+ * partition - sort an array of integers using quick_sort
+ * lomuto implementation with pivot in last element of partition
+ * @array: array to sort
+ * @min: minimum value
+ * @max: max value
+ * @size: the size of the array to sort
+ * Return: index + 1
+ */
+size_t partition(int *array, ssize_t min, ssize_t max, size_t size)
+{
+	ssize_t i, j;
+	int swap, pivot;
+
+	pivot = array[max];
+	i = min - 1;
+	for (j = min; j < max; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap = array[i];
+				array[i] = array[j];
+				array[j] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[max] < array[i + 1])
+	{
+		swap = array[i + 1];
+		array[i + 1] = array[max];
+		array[max] = swap;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quicksort - sorts an array (a partition recursively)
  * @array: array to be sorted
- * @size: size of array
+ * @min: min index of the partition
+ * @max: max index of the partition
+ * @size: array size
+ */
+void quicksort(int *array, ssize_t min, ssize_t max, size_t size)
+{
+	ssize_t pivot;
+
+	if (min < max)
+	{
+		pivot = partition(array, min, max, size);
+		quicksort(array, min, pivot - 1, size);
+		quicksort(array, pivot + 1, max, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array with quick sort algo
+ * @array: The array to be sorted
+ * @size: The size of the array to be sorted
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 1)
+	if (array == NULL || size < 2)
 		return;
-	sort_alg(array, 0, size - 1, size);
-}
-
-/**
- * sort_alg - recursive sorting algorithm
- * @arr: array
- * @left: leftmost index
- * @right: rightmost index
- * @size: full size of array
- */
-void sort_alg(int *arr, int left, int right, size_t size)
-{
-	int pivot;
-
-	if (left < right)
-	{
-		pivot = split(arr, left, right, size);
-		sort_alg(arr, left, pivot - 1, size);
-		sort_alg(arr, pivot + 1, right, size);
-	}
-}
-
-/**
- * split - split array
- * @arr: array
- * @left: leftmost index
- * @right: rightmost index
- * @size: full array size
- * Return: pivot index
- */
-int split(int *arr, int left, int right, size_t size)
-{
-	int i, i2, pivot, tmp;
-
-	pivot = arr[right];
-	i = left;
-
-	for (i2 = left; i2 < right; i2++)
-	{
-		if (arr[i2] < pivot)
-		{
-			if (i != i2)
-			{
-				tmp = arr[i2];
-				arr[i2] = arr[i];
-				arr[i] = tmp;
-				print_array(arr, siz);
-			}
-			i++;
-		}
-	}
-	if (arr[i] != arr[right])
-	{
-		tmp = arr[i];
-		arr[i] = arr[right];
-		arr[right] = tmp;
-		print_array(arr, size);
-	}
-	return (i);
+	quicksort(array, 0, size - 1, size);
 }
